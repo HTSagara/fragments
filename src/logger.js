b5156f11fcfa4d40ef532265582c3225a3def068
Henrique <henrique.sagara@hotmail.com>
2024-08-02 17:41:27 -0400

    turn off pretty printer for production

--- src/logger.js
+++ src/logger.js
@@ -3,15 +3,17 @@
 // Use `info` as our standard log level if not specified
 const options = { level: process.env.LOG_LEVEL || 'info' };
 
-// If we're doing `debug` logging, make the logs easier to read
-if (options.level === 'debug') {
-  // https://github.com/pinojs/pino-pretty
-  options.transport = {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  };
+if (process.env.NODE_ENV !== 'production') {
+  // If we're doing `debug` logging, make the logs easier to read
+  if (options.level === 'debug') {
+    // https://github.com/pinojs/pino-pretty
+    options.transport = {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+      },
+    };
+  }
 }
 
 // Create and export a Pino Logger instance:
